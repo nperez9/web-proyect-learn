a6b6293a632b8701f40f147edb2318778121de30
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2024-10-13 22:44:45 -0300

    feat: added stuff

--- Python/fast-api-postgresql/main.py
+++ Python/fast-api-postgresql/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from models.user_connetion import UserConnection
 
 app = FastAPI()
+user_connection = UserConnection()
 
 @app.get("/")
 def root():
+    user_connection
     return {"Hello": "World"}
